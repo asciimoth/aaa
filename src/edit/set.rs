2ec4a3e58571ff2a531f2ec9c34b15fb10ecbe1b
AsciiMoth <ascii@moth.contact>
2026-02-28 18:20:01 +0400

    chore: add gpl license headers

--- src/edit/set.rs
+++ src/edit/set.rs
@@ -1,6 +1,22 @@
+/*
+    This file is part of aaa.
+
+    aaa is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    aaa is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with aaa.  If not, see <https://www.gnu.org/licenses/>.
+*/
 use anyhow::Result;
 use argh::FromArgs;
-use rs3a::{chars::Char, Cell};
+use rs3a::{Cell, chars::Char};
 
 /// Set cell (text, color)
 #[derive(FromArgs, PartialEq, Debug)]
